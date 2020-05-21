e2d462e53b5b9ed791c310586eb0200530de1094
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-21 01:26:49 -0400

    repair test setup

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,16 @@
-const enhancer = require('./enhancer.js');
+// const enhancer = require('./enhancer.js');
+const { repair } = require('./enhancer.js');
+
 // test away!
+describe("enhancer", () => {
+    describe("repair()", () => {
+
+      it("brings durability cuteness to 100", () => {
+       const meowItem = {
+           name: 'cute sword',
+           durability: 13,
+           enchancement: 7
+       }
+      });
+    });
+  });
